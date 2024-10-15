c195b8b839c6ba2f8fbbb67b68d27615a7b239af
Matheus Faculdade <matheus2015.ts@gmail.com>
2024-10-15 11:23:02 -0300

    TS-SistemaBancario - criação testes para classe de conta

--- TS-SistemaBancario/TS-SistemaBancario/testes/adsts/clientedao/ContaTeste.java
+++ TS-SistemaBancario/TS-SistemaBancario/testes/adsts/clientedao/ContaTeste.java
@@ -1,5 +1,10 @@
 package adsts.clientedao;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,37 +34,78 @@ public class ContaTeste {
     @Test
     @Order(1)
     public void testeCriarDao(){
-    	
+    	assertNotNull(cDAO);
     }
     
     @Test
     @Order(2)
     public void testeGetContasBanco(){
+    	List<ContaCorrente> listaContas = new ArrayList<>();
+    	listaContas = cDAO.getContasDoBanco();
+    	boolean existeCliente = listaContas.size()>0;
     	
+    	ContaCorrente contaModelo = new ContaCorrente(1, 1000.0, true);
+    	ContaCorrente contaPesquisada = listaContas.get(0);
+    	
+    	assertNotNull(listaContas);
+    	assertTrue(existeCliente);
+    	assertEquals(contaModelo.getId(), contaPesquisada.getId(),"ID incorreto ou nulo");
+    	assertNotNull("ID nulo", contaPesquisada.getId());
+    	assertEquals(contaModelo.getSaldo(), contaPesquisada.getSaldo(), "SALDO incorreto ou nulo");
+    	assertNotNull("SALDO nulo", contaPesquisada.getSaldo());
+    	assertEquals(contaModelo.isAtiva(), contaPesquisada.isAtiva(),"STATUS ATIVAÇÃO incorreto ou nulo");
+    	assertNotNull("STATUS ATIVAÇÃO nulo", contaPesquisada.isAtiva());
     }
     
     @Test
     @Order(3)
     public void testePesquisaConta(){
+    	ContaCorrente contaModelo = new ContaCorrente(1, 1000.0, true);
+    	ContaCorrente contaPesquisada = cDAO.pesquisaConta(1);
     	
+    	assertNotNull("Conta não existente/nulo", contaPesquisada);
+    	assertEquals(contaModelo.getId(), contaPesquisada.getId(),"ID incorreto ou nulo");
+    	assertNotNull("ID nulo", contaPesquisada.getId());
+    	assertEquals(contaModelo.getSaldo(), contaPesquisada.getSaldo(), "SALDO incorreto ou nulo");
+    	assertNotNull("SALDO nulo", contaPesquisada.getSaldo());
+    	assertEquals(contaModelo.isAtiva(), contaPesquisada.isAtiva(),"STATUS ATIVAÇÃO incorreto ou nulo");
+    	assertNotNull("STATUS ATIVAÇÃO nulo", contaPesquisada.isAtiva());
     }
     
     @Test
     @Order(1)
     public void testeAdicionarConta(){
+    	ContaCorrente contaNova = new ContaCorrente(3, 1200.0, true);
+    	int tamanhoListaContasAntes = cDAO.getContasDoBanco().size();
+    	cDAO.adicionaConta(contaNova);
+    	int tamanhoListaContasDepois = cDAO.getContasDoBanco().size();
+    	assertEquals(tamanhoListaContasAntes + 1, tamanhoListaContasDepois);
     	
+    	ContaCorrente contaPesquisada = cDAO.pesquisaConta(3);
+    	
+    	assertNotNull("Conta não existente/nulo", contaPesquisada);
+    	assertEquals(contaNova.getId(), contaPesquisada.getId(),"ID incorreto ou nulo");
+    	assertNotNull("ID nulo", contaPesquisada.getId());
+    	assertEquals(contaNova.getSaldo(), contaPesquisada.getSaldo(), "SALDO incorreto ou nulo");
+    	assertNotNull("SALDO nulo", contaPesquisada.getSaldo());
+    	assertEquals(contaNova.isAtiva(), contaPesquisada.isAtiva(),"ST	ATUS ATIVAÇÃO incorreto ou nulo");
+    	assertNotNull("STATUS ATIVAÇÃO nulo", contaPesquisada.isAtiva());
     }
     
     @Test
     @Order(1)
     public void testeRemoveConta(){
-    	
+    	int tamanhoListaContasAntes = cDAO.getContasDoBanco().size();
+    	cDAO.removeConta(3);
+    	int tamanhoListaContasDepois = cDAO.getContasDoBanco().size();
+    	assertEquals(tamanhoListaContasAntes - 1, tamanhoListaContasDepois, "Conta não foi removido");
+    	assertNull(cDAO.pesquisaConta(3));
     }
     
     @Test
     @Order(1)
     public void testeContaAtiva(){
-    	
+    	assertTrue(cDAO.contaAtiva(1));
     }
     
     @Test
